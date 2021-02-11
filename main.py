d6cd8082bd9875a5a73e8d7e193ca76b9f978e27
AasawariBhavsar <aasawari1234bhavsar@gmail.com>
2021-02-11 16:22:31 +0530

    update main.py requirements.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 from email_validator import validate_email
-
+import os
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -40,12 +40,12 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
